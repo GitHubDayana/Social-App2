f9f724bd33aaabbd296c8c3e3f459ff289bdd8a2
M Vijay Kumar <manohar.vijay001@gmail.com>
2024-10-07 20:16:39 +0530

    code commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+
+const app = express();
+const port = 3000;
+
+// MySQL connection setup
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'sai.prasad@155',
+    database: 'social_app'
+});
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('MySQL Connected...');
+});
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Routes
+app.use('/user', require('./routes/user'));
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
